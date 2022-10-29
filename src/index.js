6794547c5fa6d1042cc8957033fb38b8ac4668ff
Olevova <olevova1983@gmail.com>
2022-10-29 20:59:46 +0300

    many

--- src/index.js
+++ src/index.js
@@ -1,27 +1,44 @@
 import './css/styles.css';
 import contryTemple from './template/contryshablon.hbs';
-import hbs from "handlebars";
+import contryMany from './template/manycountry.hbs'
+import Notiflix from 'notiflix';
 
 import fetchCountries from "./fetchCountries";
 
 
 const eleCreator = ele => document.querySelector(ele);
 
-fetchCountries('uk').then(value => render(value)).catch(error => console.log(error))
+// fetchCountries('uk').then(value => render(value)).catch(error => console.log(error))
 
 eleCreator('#search-box').addEventListener('input', all);
 
 function all(e){
     let nameCountry = e.target.value;
-    console.log(nameCountry);
-    fetchCountries(nameCountry).then(value => render(value)).catch(error => console.log(error))
+    console.log(nameCountry, nameCountry.length);
+    if(nameCountry.length > 0)
+        fetchCountries(nameCountry).then(value => render(value)).catch(error => console.log(error))
+    else {
+        eleCreator('.country-info').innerHTML = '';
+    }
 }
 
 function render(value) {
     console.log(value);
-    const card = contryTemple({value});
-    console.log(card);
-    eleCreator('.country-info').insertAdjacentHTML("beforeend", card);
+    if (value.length > 2 && 10 > value.length) {
+        console.log(1111);
+        const card = contryMany({ value });
+        return eleCreator('.country-info').innerHTML = card;
+    }
+    else if (value.length === 1) {
+        const card = contryTemple({ value });
+        console.log(card);
+        return eleCreator('.country-info').innerHTML = card;
+    }
+    // eleCreator('.country-info').insertAdjacentHTML("beforeend", card)
+    else {
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        eleCreator('.country-info').innerHTML = '';
+    }
 }
 
 // const conries = [
